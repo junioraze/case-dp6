4db33b7a14b0e06945da93b054dd14c605c33207
junioraze <junioraze2@gmail.com>
2020-12-18 14:24:17 -0200

    tagueamento2

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -7,13 +7,13 @@ event_menu_contato.addEventListener("click", function() {
   console.log("evento_menu_contato_clicado")
 }, false);
 
-event_menu_contato.addEventListener("click", function() {
+event_menu_pdf.addEventListener("click", function() {
   window.ga("send", "event", "menu", "download_pdf", "download_pdf");
   console.log("evento_menu_pdf_clicado")
 }, false);
 
 // criacao de eventos especificos para a pagina analise.html
-if (location.pathname == "/analise.html") {
+if (location.pathname == "/case-dp6/analise.html") {
 	var event_cards = document.querySelectorAll('.card.card-montadoras')
 	
 	// funcao que adiciona um eventListener ao array de card-text's recuperado pelo .querySelectorAll
@@ -28,7 +28,7 @@ if (location.pathname == "/analise.html") {
 }
 
 // criacao de eventos especificos para a pagina analise.html
-if (location.pathname == "/sobre.html") {
+if (location.pathname == "/case-dp6/sobre.html") {
 	
 	// Evento do butao de submit criado dentro do main.js apos a chamada da classe de visualizacao da light box
 	
